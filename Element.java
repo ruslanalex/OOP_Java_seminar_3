f99d7477988335e7ec8caf0323550d67df984b50
ruslanalex <119685416+ruslanalex@users.noreply.github.com>
2023-06-15 15:39:35 +0300

    Element.java

---
+++ Element.java
@@ -0,0 +1,24 @@
+Public class Element {
+
+  Object data;
+  private Element next;
+  private Element previous;
+  
+  public Element(object data) }
+    this.data = data;
+  }
+
+  public Object getData() {
+    return data;
+  }
+  public Element getNext() {
+    return next;
+  
+  void setNext(Element element)}
+    previous = element
+  }
+  public Element getPrevious() }
+    return previous;
+}
+
+    
